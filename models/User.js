98b8c9dfa7485ad6018964b6215827b589c10e74
Phonekham <phone.madridista@gmail.com>
2019-10-27 16:06:08 +0700

    Hashing User Password

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const usersCollection = require("../db").collection("users");
 const validator = require("validator");
 
@@ -55,7 +56,10 @@ User.prototype.login = function() {
     usersCollection
       .findOne({ username: this.data.username })
       .then(attemptedUser => {
-        if (attemptedUser && attemptedUser.password == this.data.password) {
+        if (
+          attemptedUser &&
+          bcrypt.compareSync(this.data.password, attemptedUser.password)
+        ) {
           resolve("congrats");
         } else {
           reject("invalid username or password");
@@ -73,6 +77,9 @@ User.prototype.register = function() {
   this.validate();
   // Only if there is no validation errors
   if (!this.errors.length) {
+    // hash user password
+    let salt = bcrypt.genSaltSync(10);
+    this.data.password = bcrypt.hashSync(this.data.password, salt);
     usersCollection.insertOne(this.data);
   }
   // Then save the user into a database
