41f8a1c0af84a8e74d0c0ff03d52021f958157e4
Phonekham <phone.madridista@gmail.com>
2019-10-27 11:34:36 +0700

    Adding Validation To Our Model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
+const User = require("../models/User");
+
 exports.login = function() {};
 
 exports.logout = function() {};
 
 exports.register = function(req, res) {
-  res.send("register");
+  let user = new User(req.body);
+  user.register();
+  if (user.errors.length) {
+    res.send(user.errors);
+  } else {
+    res.send("congrats");
+  }
 };
 
 exports.home = function(req, res) {
---
+++ models/User.js
@@ -0,0 +1,45 @@
+const validator = require("validator");
+
+const User = function(data) {
+  this.data = data;
+  this.errors = [];
+};
+
+User.prototype.validate = function() {
+  if (this.data.username == "") {
+    this.errors.push("You must provide a username");
+  }
+  if (!validator.isEmail(this.data.email)) {
+    this.errors.push("You must provide a email address");
+  }
+  if (
+    this.data.username != "" &&
+    !validator.isAlphanumeric(this.data.username)
+  ) {
+    this.errors.push("username can contain only letters and numbers");
+  }
+  if (this.data.password == "") {
+    this.errors.push("You must provide a password");
+  }
+  if (this.data.password.length > 0 && this.data.password.lengthngth < 12) {
+    this.errors.push("password must be atleast 12");
+  }
+  if (this.data.password.length > 100) {
+    this.errors.push("password can't exceed 100");
+  }
+  if (this.data.username.length > 0 && this.data.username.length < 3) {
+    this.errors.push("username must be atleast 3");
+  }
+  if (this.data.username.length > 30) {
+    this.errors.push("username can't exceed 30");
+  }
+};
+
+User.prototype.register = function() {
+  // Validate user data
+  this.validate();
+  // Only if there is no validation errors
+  // Then save the user into a database
+};
+
+module.exports = User;
