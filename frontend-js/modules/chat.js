c6a794eac39c4370c4cb42d3e9574879c92620d2
Phonekham <phone.madridista@gmail.com>
2019-11-04 15:02:11 +0700

    chat part 1

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -4,6 +4,8 @@ export default class Chat {
     this.chatWrapper = document.querySelector("#chat-wrapper");
     this.openIcon = document.querySelector(".header-chat-icon");
     this.injectHTML();
+    this.chatField = document.querySelector("#chatField");
+    this.chatForm = document.querySelector("#chatForm");
     this.closeIcon = document.querySelector(".chat-title-bar-close");
     this.events();
   }
@@ -11,8 +13,19 @@ export default class Chat {
   events() {
     this.openIcon.addEventListener("click", () => this.showChat());
     this.closeIcon.addEventListener("click", () => this.hideChat());
+    this.chatForm.addEventListener("submit", e => {
+      e.preventDefault();
+      this.sendMessageToServer();
+    });
   }
   // Methods
+  sendMessageToServer() {
+    this.socket.emit("chatMessageFromBrowser", {
+      message: this.chatField.value
+    });
+    this.chatField.value = "";
+    this.chatField.focus();
+  }
   showChat() {
     if (!this.openedYet) {
       this.openConnection();
@@ -22,6 +35,9 @@ export default class Chat {
   }
   openConnection() {
     this.socket = io();
+    this.socket.on("chatMessageFromServer", function(data) {
+      alert(data.message);
+    });
   }
 
   hideChat() {
@@ -30,7 +46,7 @@ export default class Chat {
   injectHTML() {
     this.chatWrapper.innerHTML = `
     <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
-    <div id='chat' class='chat- log></div>
+    <div id='chat' class='chat-log'></div>
 
     <form id="chatForm" class="chat-form border-top">
       <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
